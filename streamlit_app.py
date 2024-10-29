00a7f68a5cd5629765f21b4c7479d479cd97c5ef
jorgenevesptm <jorgenevesptm@gmail.com>
2024-10-29 16:52:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,6 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import datetime as dt
 
-username = 'private_neves_jorge'
-password = 'NEtUT99yp5'
-
-coordinates = [(47.11, 11.47)]
-parameters = ['t_2m:C', 'precip_1h:mm', 'wind_speed_10m:ms']
-model = 'mix'
-startdate = dt.datetime.utcnow().replace(minute=0, second=0, microsecond=0)
-enddate = startdate + dt.timedelta(days=1)
-interval = dt.timedelta(hours=1)
-
-df = api.query_time_series(coordinates, startdate, enddate, interval, parameters, username, password, model=model)
-
 st.info(df)
 
 st.title('🤖 Machine Learning App')
